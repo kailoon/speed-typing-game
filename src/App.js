589994fd2a94ee7a7426db2cc7139e42855ff046
kailoon <chankailoon@gmail.com>
2020-09-09 23:43:06 +0800

    Initial release.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react"
+import wordGame from "./hooks/wordGame"
+import useWordGame from "./hooks/wordGame"
+/**
+ * Challenge:
+ *
+ * Make the input box focus (DOM elements have a method called .focus())
+ * immediately when the game starts
+ */
 
 function App() {
+  const {
+    inputRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount,
+  } = useWordGame()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+        ref={inputRef}
+      />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h1>Word count: {wordCount}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
